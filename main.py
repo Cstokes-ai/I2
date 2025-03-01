5414375a3b1bcb5ef3c53f8524a291905ba0a41c
Cornell Stokes <cdsgqc@umsystem.edu>
2025-03-01 12:31:57 -0600

    Second commit. I think I am halfway done. I jsut need to do the file matrix stuff and I should be fine for testing,

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-# This is a sample Python script.
+#This project is done in Python in Jetbrains IDE editor.
+#) https://docs.python.org/3/library/functions.html#max
+#https://www.w3schools.com/python/python_lists_comprehension.asp
+#https://www.geeksforgeeks.org/python-list-comprehension/
+#https://www.geeksforgeeks.org/python-check-if-a-file-or-directory-exists/
+#https://stackoverflow.com/questions/1904394/read-only-the-first-line-of-a-file
+import os
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def validity(line):
+    parts = line.strip().split(",")#this is to check if the file has proper syntax structure for the reading
+    if len(parts) < 2 or len(parts) >= 100:
+        print("The file does not have the correct syntax structure.")
+        return False
+    if not all (part.isdigit() for part in parts):
+        print("The file does not have the correct syntax structure.")
+        return False
+    return True
 
-
+def file_search():
+    path = os.getcwd()
+    while True:
+        file_name = input("Enter the name of the file you are looking for: ")
+        if os.path.isfile(path):
+            with open(file_name, "r") as file:
+                for line in file:
+                    if validity(line):
+                        return file_name,list(map(int, line.split(",")))
+        else:
+            print("The file does not exist. Please try again.")
+            continue
 
 
 def main():
